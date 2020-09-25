2ca0df60fb3a3852d5b68a504b724e3eb1eab21c
alicianunezisaac <alicianunez10492@gmail.com>
2020-09-26 01:04:19 +0200

    CSS 90'S Styled

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,11 @@
-// console.log('Hola Mundo🖐🏻');
+const width = 10;
+const height = 10;
+const cellCount = width * height;
+console.log(cellCount);
+const grid = document.querySelector('.grid');
+for (let index = 0; index < cellCount; index = index + 1) {
+  console.log(index);
+  const cell = document.createElement('div');
+  cell.innerText = index;
+  grid.appendChild(cell);
+}
