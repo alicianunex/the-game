4cd5cc53cf35946725eddf7b11f6677dfc638dd5
alicianunezisaac <alicianunez10492@gmail.com>
2020-09-29 17:40:36 +0200

    cambios

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 /**************************************** */
-// Grilla
+// Grid
 /**************************************** */
 const width = 10;
 const height = 10;
@@ -11,7 +11,7 @@ const cells = [];
 for (let index = 0; index < cellCount; index = index + 1) {
   // 1.1 Crea un elemento nuevo (div)
   const cell = document.createElement('div');
-  // 1.2 Lo coloca como hijo del elemento con la clase grid (linea 16 en HTM)
+  // 1.2 Lo coloca como hijo del elemento con la clase grid (linea 16 en HTML)
   grid.appendChild(cell);
   // 1.3 Coloca el elemento nuevo dentro de una colección (array) llamada cells
   cells.push(cell);
@@ -26,7 +26,7 @@ let shooterPosition = 94;
 const addshooter = (index) => cells[index].classList.add('shooter');
 const removeshooter = (index) => cells[index].classList.remove('shooter');
 
-// 2.1 Esta función maneja el evento de presion de teclas
+// 2.1 Esta función maneja el evento de presión de teclas
 const handleKeyPress = (event) => {
   // Haciendo desestructuración coloco en una constante llamada key el valor de la propiedad key
   // del objecto event
@@ -37,42 +37,42 @@ const handleKeyPress = (event) => {
   const x = shooterPosition % 10;
   const y = Math.floor(shooterPosition / 10);
 
-  // Antes de evaluar que tecla presiono el usuario remuevo el shooter de la posicion actual
+  // Antes de evaluar que tecla presionó el usuario remuevo el shooter de la posicion actual
   // Esto es para crear la ilusion de movimiento del shooter en pantalla
 
   removeshooter(shooterPosition);
 
-  // Evaluo que tecla toco el usuario
+  // Evaluo que tecla tocó el usuario
   switch (key) {
     // Si es la flecha derecha
     case 'ArrowRight':
-      // compara si la posicion esta en dentro del margen derecho
+      // compara si la posición esta en dentro del margen derecho
       if (x < width - 1) {
-        // Suma + 1 a la posicion
-        // ej: posicion inicial era 55 la nueva sera 56
+        // Suma + 1 a la posición
+        // ej: posición inicial era 55 la nueva será 56
         shooterPosition++;
       }
       break;
     // Si es la flecha izquierda
     case 'ArrowLeft':
-      // compara si la posicion esta en dentro del margen izquierdo
+      // compara si la posición esta en dentro del margen izquierdo
       if (x > 0) {
-        // resta - 1 a la posicion
-        // ej: posicion inicial era 55 la nueva sera 54
+        // resta - 1 a la posición
+        // ej: posición inicial era 55 la nueva sera 54
         shooterPosition--;
       }
       break;
     default:
       break;
   }
-  // Añade el shooter a la nueva posicion
+  // Añade el shooter a la nueva posición
   addshooter(shooterPosition);
 };
 
 // Imprime por primera vez en pantalla la posicion inicial del shooter
 addshooter(shooterPosition);
 
-// 2 Escucho un evento en el navegador de presion de teclas,
+// 2 Escucho un evento en el navegador de presión de teclas,
 // cuando eso pasa ejecuto la function handleKeyPress
 window.addEventListener('keyup', handleKeyPress);
 
@@ -98,14 +98,45 @@ class Alien {
   }
 }
 
-let alienPositions = [42, 43, 44, 45, 46, 47, 52, 53, 54, 55, 56, 57];
+let alienPositions = [
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  22,
+  23,
+  24,
+  25,
+  26,
+  27,
+  32,
+  33,
+  34,
+  35,
+  36,
+  37,
+  42,
+  43,
+  44,
+  45,
+  46,
+  47,
+  52,
+  53,
+  54,
+  55,
+  56,
+  57,
+];
 
 const addalien = (index) => cells[index].classList.add('alien');
 const removealien = (index) => cells[index].classList.remove('alien');
 
 let aliens = [];
 
-// 3.1 Ejecuta la funcion addAliensToBoard por cada uno de las posiciones dentro
+// 3.1 Ejecuta la función addAliensToBoard por cada una de las posiciones dentro
 // de la colección(array) alienPositions
 const addAliensToBoard = (initialPosition) => {
   // 3.2 Crea un objeto desde la plantilla Alien
@@ -117,7 +148,7 @@ const addAliensToBoard = (initialPosition) => {
   aliens.push(alien);
 };
 
-// 3 Imprimie aliens en pantalla
+// 3 Imprime aliens en pantalla
 alienPositions.forEach(addAliensToBoard);
 
 /**************************************** */
@@ -127,14 +158,14 @@ alienPositions.forEach(addAliensToBoard);
 // 4. Para tener el efecto de que los aliens se mueven debo:
 const moveToRight = () => {
   // 4.1 Crear un un nuevo array con las nuevas posiciones,
-  // para luego sustuir el valor de las posiciones iniciales que vienen del array alienPositions con nuevas posiciones
+  // para luego sustituir el valor de las posiciones iniciales que vienen del array alienPositions con nuevas posiciones
   const newAlienPositions = [];
-  // como me muevo a la derecha, debo saber cual es el valor de la posicion mas alta
+  // como me muevo a la derecha, debo saber cual es el valor de la posición más alta
   const rightPosition = aliens[aliens.length - 1].position;
-  // para evaluar si la posicion esta al margen uso las mismas condiciones que hice para handleKey function (linea 50)
+  // para evaluar si la posición esta al margen uso las mismas condiciones que hice para handleKey function (línea 50)
   const x = rightPosition % 10;
   if (x < width - 1) {
-    // Si estroy dentro del margen derecho
+    // Si estoy dentro del margen derecho
     aliens.forEach((alien) => {
       // Remuevo todos los aliens anteriores
       alien.removeFromBoard(alien.position);
@@ -143,12 +174,12 @@ const moveToRight = () => {
     });
     // Reseteo aliens
     aliens = [];
-    // Agrego nuesvos aliens al tablero y los incorporo al array alien (linea 117)
+    // Agrego nuevos aliens al tablero y los incorporo al array alien (línea 117)
     newAlienPositions.forEach(addAliensToBoard);
   }
 };
 
-// Igual que moveToRight, solo cambia los valores de margen y ce nueva posicion
+// Igual que moveToRight, solo cambia los valores del margen y de la nueva posicion
 const moveToLeft = () => {
   const newAlienPositions = [];
   const leftPosition = aliens[0].position;
@@ -181,16 +212,47 @@ const randomMovement = () => {
     // Si es par mueve a la derecha
     moveToRight();
   } else {
-    // Si es inpar mueve a la izquierda
+    // Si es impar mueve a la izquierda
     moveToLeft();
   }
 };
 
-// Ejecuta la funcion randomMovement cada cierto milisegundos
-setInterval(randomMovement, 500);
+// Ejecuta la función randomMovement cada ciertos milisegundos
+setInterval(randomMovement, 400);
+
+/**************************************** */
+// Laser
+/**************************************** */
+
+let lasertiming = 300;
+
+var laserPosition = 'laser';
+laserPosition; // arr
+
+function laser(event) {
+  let laserPosition = alienPositions;
+  const { key } = event;
+  // console.log(event.key);
+
+  switch (key) {
+    case 'w':
+      setInterval(() => {
+        removelaser(laserPosition);
+        laserPosition -= width;
+        addlaser(laserPosition);
+        console.log('laser');
+      }, 300);
+  }
+}
+
+console.log(laserPosition);
+
+const addlaser = (index) => cells[index].classList.add('laser');
+const removelaser = (index) => cells[index].classList.remove('laser');
+
+window.addEventListener('keypress', laser);
 
 // TODO:
 
-// Disparar laser
 // Desaparecer aliens cuando sean disparados
 // Score
